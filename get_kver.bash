f3c3aa3aad35e66fcee6629a9c596849d1d28264
Joe Landman <joe@nlytiq.com>
2019-04-16 01:36:01 -0400

    Fixing an incorrect motd and kver issue for ubuntu18.04

--- get_kver.bash
+++ get_kver.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 T=$1
-dpkg -l --root=$T | grep linux-image- | grep -v meta | perl -lane 'print $F[1] =~ /linux-image-(.*)/'
+dpkg -l --root=$T | grep linux-image- | grep -v meta | head -1 | perl -lane 'print $F[1] =~ /linux-image-(.*)/'
